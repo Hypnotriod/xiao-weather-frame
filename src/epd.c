715af36cbceaaa6bad5e248fdb7420097250e01f
illia <illia.pikin@gmail.com>
2026-05-28 08:55:30 +0300

    Fix: off by one

--- src/epd.c
+++ src/epd.c
@@ -464,8 +464,10 @@ void epd_draw_image(const uint8_t* image, uint8_t* buffer, uint16_t width, uint1
                     buffer[byte_offset] |= mask[1];
                 }
             }
+            if (byte_offset != 0 || pixel_x >= 0) {
+                byte_offset += 1;
+            }
             pixel_x += 8;
-            byte_offset += 1;
             if (pixel_x < EPD_WIDTH && pixel_x >= 0) {
                 if (color == EPD_COLOR_BLACK) {
                     if (!transparent)
